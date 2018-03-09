c897885c8f459f09eb81cc65ff8401d64326492f
Sanjib Narzary <o-._.-o@live.com>
2018-03-09 20:27:06 +0530

    Bodo specific commit

--- nmt/scripts/download_iwslt15.sh
+++ nmt/scripts/download_iwslt15.sh
@@ -7,24 +7,24 @@
 #
 # If output directory is not specified, "./iwslt15" will be used as the default
 # output directory.
-OUT_DIR="${1:-iwslt15}"
-SITE_PREFIX="https://nlp.stanford.edu/projects/nmt/data"
+OUT_DIR="${1:-enbrxmed18}"
+SITE_PREFIX="http://cit.ac.in/~sn"
 
 mkdir -v -p $OUT_DIR
 
 # Download iwslt15 small dataset from standford website.
-echo "Download training dataset train.en and train.vi."
-curl -o "$OUT_DIR/train.en" "$SITE_PREFIX/iwslt15.en-vi/train.en"
-curl -o "$OUT_DIR/train.vi" "$SITE_PREFIX/iwslt15.en-vi/train.vi"
+echo "Download training dataset train.en and train.brx."
+curl -o "$OUT_DIR/train.en" "$SITE_PREFIX/training.en-brx.tok.en"
+curl -o "$OUT_DIR/train.brx" "$SITE_PREFIX/training.en-brx.tok.brx"
 
-echo "Download dev dataset tst2012.en and tst2012.vi."
-curl -o "$OUT_DIR/tst2012.en" "$SITE_PREFIX/iwslt15.en-vi/tst2012.en"
-curl -o "$OUT_DIR/tst2012.vi" "$SITE_PREFIX/iwslt15.en-vi/tst2012.vi"
+echo "Download dev dataset test.en and test.brx."
+curl -o "$OUT_DIR/test.en" "$SITE_PREFIX/test.tok.en"
+curl -o "$OUT_DIR/test.brx" "$SITE_PREFIX/test.tok.brx"
 
 echo "Download test dataset tst2013.en and tst2013.vi."
-curl -o "$OUT_DIR/tst2013.en" "$SITE_PREFIX/iwslt15.en-vi/tst2013.en"
-curl -o "$OUT_DIR/tst2013.vi" "$SITE_PREFIX/iwslt15.en-vi/tst2013.vi"
+curl -o "$OUT_DIR/tst2013.en" "$SITE_PREFIX/development.tok.en"
+curl -o "$OUT_DIR/tst2013.brx" "$SITE_PREFIX/development.tok.brx"
 
 echo "Download vocab file vocab.en and vocab.vi."
-curl -o "$OUT_DIR/vocab.en" "$SITE_PREFIX/iwslt15.en-vi/vocab.en"
-curl -o "$OUT_DIR/vocab.vi" "$SITE_PREFIX/iwslt15.en-vi/vocab.vi"
+curl -o "$OUT_DIR/vocab.en" "$SITE_PREFIX/vocab.50k.en"
+curl -o "$OUT_DIR/vocab.brx" "$SITE_PREFIX/vocab.50k.brx"
