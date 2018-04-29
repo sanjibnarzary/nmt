d3e1d5fe38427b629932865d9053eef31b394706
Sanjib Narzary <o-._.-o@live.com>
2018-04-29 15:52:53 +0530

    Updated with tourism data for en brx nmt

--- nmt/scripts/download_iwslt15.sh
+++ nmt/scripts/download_iwslt15.sh
@@ -8,23 +8,23 @@
 # If output directory is not specified, "./iwslt15" will be used as the default
 # output directory.
 OUT_DIR="${1:-enbrxmed18}"
-SITE_PREFIX="https://cit.ac.in/~sn"
+SITE_PREFIX="https://cit.ac.in/~sn/data/ebnmt"
 
 mkdir -v -p $OUT_DIR
 
 # Download iwslt15 small dataset from standford website.
 echo "Download training dataset train.en and train.brx."
-curl -o "$OUT_DIR/train.en" "$SITE_PREFIX/training.en-brx.tok.en"
-curl -o "$OUT_DIR/train.brx" "$SITE_PREFIX/training.en-brx.tok.brx"
+curl -o "$OUT_DIR/train.en" "$SITE_PREFIX/training.en"
+curl -o "$OUT_DIR/train.brx" "$SITE_PREFIX/training.brx"
 
 echo "Download dev dataset test.en and test.brx."
-curl -o "$OUT_DIR/test.en" "$SITE_PREFIX/test.tok.en"
-curl -o "$OUT_DIR/test.brx" "$SITE_PREFIX/test.tok.brx"
+curl -o "$OUT_DIR/tst2012.en" "$SITE_PREFIX/test.en"
+curl -o "$OUT_DIR/tst2012.brx" "$SITE_PREFIX/test.brx"
 
-echo "Download test dataset tst2013.en and tst2013.vi."
-curl -o "$OUT_DIR/tst2013.en" "$SITE_PREFIX/development.tok.en"
-curl -o "$OUT_DIR/tst2013.brx" "$SITE_PREFIX/development.tok.brx"
+echo "Download test dataset tst2013.en and tst2013.brx."
+curl -o "$OUT_DIR/tst2013.en" "$SITE_PREFIX/dev.en"
+curl -o "$OUT_DIR/tst2013.brx" "$SITE_PREFIX/dev.brx"
 
-echo "Download vocab file vocab.en and vocab.vi."
+echo "Download vocab file vocab.en and vocab.brx."
 curl -o "$OUT_DIR/vocab.en" "$SITE_PREFIX/vocab.50k.en"
 curl -o "$OUT_DIR/vocab.brx" "$SITE_PREFIX/vocab.50k.brx"
